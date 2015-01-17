dfde973dc1cfb5ec58320c0961d7f1a380b6098f
stephanos <github@stephanbehnke.de>
2015-01-17 17:00:49 +0100

    add options to allow/disallow snapshot/qualified version

--- src/boot_deps.clj
+++ src/boot_deps.clj
@@ -1,10 +1,10 @@
 (ns boot-deps
   {:boot/export-tasks true}
-  (:require [clojure.java.io   :as io]
-            [boot.core         :as boot :refer [deftask]]
-            [boot.pod          :as pod]
-            [boot.file         :as file]
-            [boot.util         :as util]))
+  (:require [clojure.java.io :as io]
+    [boot.core :as boot :refer [deftask]]
+    [boot.pod :as pod]
+    [boot.file :as file]
+    [boot.util :as util]))
 
 (def initial
   (atom true))
@@ -12,23 +12,27 @@
 (defn make-ancient-pod []
   (pod/make-pod (assoc (boot/get-env) :dependencies '[[ancient-clj "0.2.1"]])))
 
-(defn find-outdated [env]
+(defn find-outdated [env opts]
   (let [ancient-pod (make-ancient-pod)
         {:keys [dependencies repositories]} env]
     (pod/with-eval-in ancient-pod
       (require '[ancient-clj.core :as ancient])
       (let [deps ~(mapv #(list 'quote %) dependencies)
             artifacts (map ancient/read-artifact deps)
-            outdated (map ancient/artifact-outdated? deps)]
+            outdated (map #(ancient/artifact-outdated? % opts) deps)]
         (->> (map vector artifacts outdated)
           (filter #(identity (second %))))))))
 
 (deftask ancient
   "Find outdated dependencies"
-  []
+  [s allow-snapshots  bool  "allow SNAPSHOT versions to be reported as new"
+   q allow-qualified  bool  "allow alpha, beta, etc... versions to be reported as new"
+   a allow-all        bool  "allow SNAPSHOT and qualified versions to be reported as new"]
   (boot/with-pre-wrap fileset
     (util/info "Searching for outdated dependencies...\n")
-    (doseq [[artifact new] (find-outdated (boot/get-env))]
-      (util/info "Currently using %s but %s is available\n"
-        (pr-str (:form artifact)) (:version-string new)))
+    (let [opts {:snapshots? (or allow-snapshots allow-all)
+                :qualified? (or allow-qualified allow-all)}]
+      (doseq [[artifact new] (find-outdated (boot/get-env) opts)]
+        (util/info "Currently using %s but %s is available\n"
+          (pr-str (:form artifact)) (:version-string new))))
     fileset))
